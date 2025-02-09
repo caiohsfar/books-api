36494d15832a0849eac3693328ca4d15bff2c507
Rute Cajueiro <ruthynhas344@gmail.com>
2025-02-09 18:19:27 -0300

    feat: load csv into database

---
+++ src/main/resources/db/migration/V2__create.sql
@@ -0,0 +1,12 @@
+CREATE TABLE book (
+    id BIGINT PRIMARY KEY,
+    title TEXT,
+    author TEXT,
+    main_genre TEXT,
+    sub_genre TEXT,
+    type TEXT,
+    price TEXT,
+    rating FLOAT,
+    no_people_rated FLOAT,
+    url TEXT
+);
